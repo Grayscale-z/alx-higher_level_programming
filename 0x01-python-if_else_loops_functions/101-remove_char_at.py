03f5eadd8a44e43dbeaee31cd240fe069d6ac6f0
Ezenwa <Ndubuzorezenwa@gmail.com>
2022-12-01 02:07:59 +0000

    All tasks

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,6 +2,6 @@
 # Author - Ezenwa Ndubuzor
 
 def remove_char_at(str, n):
-if n < 0:
-return (str)
-return (str[:n] + str[n+1:])
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
